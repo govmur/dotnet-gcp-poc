e4e2725d6de4c1ac772a08c741930f66ac339f97
Govind <govmur@nasdaq.com>
2022-09-27 17:23:40 +0530

    Initial setup: Hello World

---
+++ HelloWorldRestAPI/HelloWorldRestAPI/Controllers/HelloWorldController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorldRestAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Good Morning", "Good Afternoon", "Good Evening","Good Night","Good Day"
+    };
+
+        private readonly ILogger<HelloWorldController> _logger;
+
+        public HelloWorldController(ILogger<HelloWorldController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetGreeting")]
+        public HelloWorld Get()
+        {
+            var greeting = new HelloWorld();
+            var currentTime = DateTime.Now.TimeOfDay.Hours;
+            var greetingText = string.Empty;
+            if (currentTime < 12)
+                greetingText = Summaries[0];
+            else if (currentTime >= 12 && currentTime < 15)
+                greetingText = Summaries[1];
+            else if (currentTime >= 15 && currentTime < 20)
+                greetingText = Summaries[2];
+            else if (currentTime >= 20 && currentTime < 24)
+                greetingText = Summaries[3];
+            else
+                greetingText = Summaries[4];
+
+            greeting.Message = greetingText + ", Welcome to One Report GCP PoC!";
+            greeting.Date = DateTime.Now;
+            greeting.Hour = currentTime;
+           
+            return greeting;
+        }
+    }
+}
---
+++ HelloWorldRestAPI/HelloWorldRestAPI/HelloWorld.cs
@@ -0,0 +1,11 @@
+namespace HelloWorldRestAPI
+{
+    public class HelloWorld
+    {
+        public DateTime Date { get; set; }
+
+        public string Message { get; set; }
+
+        public int Hour { get; set; }
+    }
+}
